c8985365d6691da39ad9f0138d5827c52a943c85
joshua-bell11 <joshua.bell11@tamu.edu>
2020-09-11 12:20:49 -0500

    Simplification

--- main.py
+++ main.py
@@ -1,108 +1,107 @@
 #!/usr/bin/env python3
 
 from PIL import Image, ImageDraw, ImageFont
-import time
 from tqdm import tqdm
 
-def maxCharSize(size, fontFile="Lato-Regular.ttf"):
-    # Get the max pixel size of a character of a specific font and size.
-    with open("quotes.txt", "r") as file:
-        chars = set(list(file.read()))
+class quote:
+    def __init__(self):
+        self.quoteFile = "quotes.txt" # Text file of quotes
+        self.fontFile = "Lato-Regular.ttf" # Font .ttf file
+        self.fontSize = 50 # Size of the font.
+        self.maxLetters = 70 # Maximum amount of characters in one line.
+        self.resolution = (1920, 1080) # Image size/resolution.
+        self.maxSize = self.maxCharSize()
+
+    def bulkProcess(self):
+        # Process all quotes
+        with open(self.quoteFile, "r") as file:
+            lines = [i.replace("\n", "") for i in file.readlines() if i != "" and i != "\n"]
+            lines.reverse()
+        
+        with tqdm(total=len(lines), unit="image", desc="Image Generation") as pbar:
+            for idx, val in enumerate(lines):
+                self.makeImage(val, idx)
+                pbar.update(1)
+
+    def singleProcess(self):
+        # Process single quote
+        self.makeImage(input("Quote: "))
     
-    font = ImageFont.truetype(fontFile, size)
+    def maxCharSize(self) -> dict:
+        # Get max possible pixel size of a single character.
+        with open(self.quoteFile, "r") as file:
+            chars = set(list(file.read()))
 
-    heights = []
-    widths = []
-    for c in chars:
-        w, h = font.getsize(c)
-        heights.append(h)
-        widths.append(w)
+        font = ImageFont.truetype(self.fontFile, self.fontSize)
+        heights, widths = [], []
+        for c in chars:
+            w, h = font.getsize(c)
+            heights.append(h)
+            widths.append(w)
 
-    return {
-        "height": max(heights),
-        "width": max(widths)
-    }
-
-def lineWidth(text, size=50, fontFile="Lato-Regular.ttf"):
-    # Get string pixel width
-    font = ImageFont.truetype(fontFile, size)
-    w, h = font.getsize(text)
-    return w
-
-def multiline(text):
-    # Calculate multiple lines, output as string with \n and spaces
-    allowedWidth = 1920 - (50 * 2)
-    # maxLetters = int(allowedWidth / maxCharSize(50)["width"]) # Max letters allowed per line
-    maxLetters = 70
-
-    text = text.replace("  ", " ").replace("\n", "")
-    
-    if len(text) <= maxLetters: # If quote can fit into one sentence.
         return {
-            "text": text,
-            "height": maxCharSize(50)["height"],
-            "width": lineWidth(text)
+            "height": max(heights),
+            "width": max(widths)
         }
-    else: # If quote cannot fit into one sentence.
-        currentIdx = maxLetters - 1
-        seperator = "\n   "
-        lines = []
-        while True:
-            if currentIdx < len(text) and len(text) >= maxLetters:
-                if text[currentIdx] == " ":
-                    lines.append(text[:currentIdx] + seperator)
-                    text = text[currentIdx:]
-                    currentIdx = maxLetters - 1
+    
+    def lineWidth(self, text: str) -> int:
+        # Get string pixel width
+        font = ImageFont.truetype(self.fontFile, self.fontSize)
+        return font.getsize(text)[0]
+
+    def multiline(self, text: str) -> dict:
+        text = text.replace("  ", " ").replace("\n", "")
+
+        if len(text) <= self.maxLetters: # If quote can fit into one sentence.
+            return {
+                "text": text,
+                "height": self.maxCharSize()["height"],
+                "width": self.lineWidth(text)
+            }
+        else: # If quote cannot fit into one sentence.
+            currentIdx = self.maxLetters - 1
+            seperator = "\n   "
+            lines = []
+            while True:
+                if currentIdx < len(text) and len(text) >= self.maxLetters:
+                    if text[currentIdx] == " ":
+                        lines.append(text[:currentIdx] + seperator)
+                        text = text[currentIdx:]
+                        currentIdx = self.maxLetters - 1
+                    else:
+                        currentIdx -= 1
+                        continue
+                elif len(text) <= self.maxLetters:
+                    lines.append(text)
+                    break
                 else:
-                    currentIdx -= 1
-                    continue
-            elif len(text) <= maxLetters:
-                lines.append(text)
-                break
-            else:
-                break
+                    break
+        
+            return {
+                "text": "".join(lines),
+                "height": self.maxCharSize()["height"] * len(lines),
+                "width": max([self.lineWidth(i) for i in lines])
+            }
     
-        return {
-            "text": "".join(lines),
-            "height": maxCharSize(50)["height"] * len(lines),
-            "width": max([lineWidth(i) for i in lines])
-        }
-                
-def makeImage(text, textHeight, textWidth, outFile="test.png"):
-    # Make an image
-    image = Image.open("blank.png")
-    draw = ImageDraw.Draw(image)
-    imWidth, imHeight = image.size
-    centerX, centerY = imWidth / 2, imHeight / 2
+    def makeImage(self, text: str, outFile: str="test"):
+        # Make an image
+        image = Image.new("RGB", self.resolution, color="black")
+        draw = ImageDraw.Draw(image)
+        centerX, centerY = self.resolution[0] / 2, self.resolution[1] / 2
 
-    font = ImageFont.truetype("Lato-Regular.ttf", 50)
-    draw.text(
-        (
-            centerX - (textWidth / 2), # X Coord (Top left)
-            centerY - (textHeight / 2) # Y Coord (Top left)
-        ), 
-        text, 
-        font=font, 
-        align="left"
-        )
-    image.save("images/{}".format(outFile))
+        font = ImageFont.truetype(self.fontFile, self.fontSize)
+        lines = self.multiline(text)
+        draw.text(
+            (
+                centerX - (lines["width"] / 2), # X Coord (Top left)
+                centerY - (lines["height"] / 2) # Y Coord (Top left)
+            ), 
+            lines["text"], 
+            font=font,
+            align="left"
+            )
 
-def bulkProcess():
-    # Process all quotes
-    with open("quotes.txt", "r") as file:
-        lines = [i.replace("\n", "") for i in file.readlines() if i != "" and i != "\n"].reverse()
-
-    with tqdm(total=len(lines), unit="image", desc="Image Generation") as pbar:
-        for idx, text in enumerate(lines):
-            m = multiline(text)
-            makeImage(text=m["text"], textHeight=m["height"], textWidth=m["width"], outFile="{}.png".format(idx))
-            pbar.update(1)
-            # print("{} of {} completed".format(idx+1, len(lines)))
-
-def singleProcess(text):
-    # Process single quote
-    m = multiline(text)
-    makeImage(text=m["text"], textHeight=m["height"], textWidth=m["width"])
+        image.save("images/{}.png".format(outFile))
 
 if __name__ == "__main__":
-    bulkProcess()
+    quote().bulkProcess()
