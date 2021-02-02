bc776886a7f9b6b967f3b19e624e4a42224a921b
joshua-bell11 <joshua.bell11@tamu.edu>
2021-02-01 22:39:22 -0600

    Refactoring / Modularity

--- main.py
+++ main.py
@@ -1,41 +1,36 @@
 #!/usr/bin/env python3
 
+from typing import Dict, List, Tuple
 from PIL import Image, ImageDraw, ImageFont
 from tqdm import tqdm
+import re
 
-class quote:
-    def __init__(self):
-        self.quoteFile = "quotes.txt" # Text file of quotes
-        self.fontFile = "Lato-Regular.ttf" # Font .ttf file
-        self.fontSize = 50 # Size of the font.
-        self.maxLetters = 70 # Maximum amount of characters in one line.
-        self.resolution = (1920, 1080) # Image size/resolution.
-        self.maxSize = self.maxCharSize()
+class Profanity:
+    @property
+    def badWords(self) -> List[str]:
+        with open("bad-words.txt", "r") as file:
+            return map(lambda w:w.strip().lower(), file.readlines())
 
-    def bulkProcess(self):
-        # Process all quotes
-        with open(self.quoteFile, "r") as file:
-            lines = [i.replace("\n", "") for i in file.readlines() if i != "" and i != "\n"]
-            lines.reverse()
-        
-        with tqdm(total=len(lines), unit="image", desc="Image Generation") as pbar:
-            for idx, val in enumerate(lines):
-                self.makeImage(val, idx)
-                pbar.update(1)
+    def check(self, string: str) -> bool:
+        string = string.lower()
+        for word in self.badWords:
+            if re.search(r'\b'+word+r'\b', string):
+                return word
+        return False
 
-    def singleProcess(self):
-        # Process single quote
-        self.makeImage(input("Quote: "))
-    
-    def maxCharSize(self) -> dict:
-        # Get max possible pixel size of a single character.
-        with open(self.quoteFile, "r") as file:
-            chars = set(list(file.read()))
+class ImageMaker:
+    def __init__(self, quote: str, fontFile: str="Lato-Regular.ttf", fontSize: float=50, maxLineChars: int=70, resolution: Tuple[int]=(1920,1080)) -> None:
+        self.quote = re.sub(" +", " ", quote).strip()
+        self.fontFile = fontFile
+        self.fontSize = fontSize
+        self.font = ImageFont.truetype(self.fontFile, self.fontSize)
+        self.maxLineChars = maxLineChars
+        self.resolution = resolution
 
-        font = ImageFont.truetype(self.fontFile, self.fontSize)
+    def maxCharSize(self) -> Dict[int, int]:
         heights, widths = [], []
-        for c in chars:
-            w, h = font.getsize(c)
+        for c in set(list(self.quote)):
+            w, h = self.font.getsize(c)
             heights.append(h)
             widths.append(w)
 
@@ -43,65 +38,87 @@ class quote:
             "height": max(heights),
             "width": max(widths)
         }
-    
-    def lineWidth(self, text: str) -> int:
-        # Get string pixel width
-        font = ImageFont.truetype(self.fontFile, self.fontSize)
-        return font.getsize(text)[0]
 
-    def multiline(self, text: str) -> dict:
-        text = text.replace("  ", " ").replace("\n", "")
+    def lineWidth(self, string: str) -> int:
+        return self.font.getsize(string)[0]
 
-        if len(text) <= self.maxLetters: # If quote can fit into one sentence.
+    def multiline(self) -> dict:
+        if len(self.quote) <= self.maxLineChars: # If quote can fit onto one line
             return {
-                "text": text,
+                "text": self.quote,
                 "height": self.maxCharSize()["height"],
-                "width": self.lineWidth(text)
+                "width": self.lineWidth(self.quote)
             }
-        else: # If quote cannot fit into one sentence.
-            currentIdx = self.maxLetters - 1
+        else: # If quote cannot fit onto one line
+            currentIdx = self.maxLineChars - 1
             seperator = "\n   "
             lines = []
+            
             while True:
-                if currentIdx < len(text) and len(text) >= self.maxLetters:
-                    if text[currentIdx] == " ":
-                        lines.append(text[:currentIdx] + seperator)
-                        text = text[currentIdx:]
-                        currentIdx = self.maxLetters - 1
+                if currentIdx < len(self.quote) and len(self.quote) >= self.maxLineChars:
+                    if self.quote[currentIdx] == " ":
+                        lines.append(self.quote[:currentIdx] + seperator)
+                        self.quote = self.quote[currentIdx:]
+                        currentIdx = self.maxLineChars - 1
                     else:
                         currentIdx -= 1
                         continue
-                elif len(text) <= self.maxLetters:
-                    lines.append(text)
+                elif len(self.quote) <= self.maxLineChars:
+                    lines.append(self.quote)
                     break
                 else:
                     break
-        
+            
             return {
                 "text": "".join(lines),
                 "height": self.maxCharSize()["height"] * len(lines),
                 "width": max([self.lineWidth(i) for i in lines])
             }
-    
-    def makeImage(self, text: str, outFile: str="test"):
-        # Make an image
+
+    def save(self, outFile="test"):
         image = Image.new("RGB", self.resolution, color="black")
         draw = ImageDraw.Draw(image)
         centerX, centerY = self.resolution[0] / 2, self.resolution[1] / 2
 
-        font = ImageFont.truetype(self.fontFile, self.fontSize)
-        lines = self.multiline(text)
+        lines = self.multiline()
         draw.text(
             (
-                centerX - (lines["width"] / 2), # X Coord (Top left)
-                centerY - (lines["height"] / 2) # Y Coord (Top left)
-            ), 
-            lines["text"], 
-            font=font,
+                centerX - (lines["width"] / 2), # X-Coord (Top Left)
+                centerY - (lines["height"] / 2) # Y-Coord (Top Left)
+            ),
+            lines["text"],
+            font=self.font,
             align="left"
-            )
+        )
 
-        image.save("images/{}.png".format(outFile))
+        image.save("images/"+outFile+".png")
+
+class Process:
+    def __init__(self, censor=False) -> None:
+        self.censor = censor
+
+    def single(self):
+        quote = input("Quote: ")
+        if self.censor and Profanity().check(quote):
+            print("Profanity found, aborting")
+            return None
+
+        ImageMaker(quote).save()    
+
+    def bulk(self, quoteFile: str):
+        with open(quoteFile, "r") as file:
+            quotes = [i.strip() for i in file.readlines() if i.strip() != ""]
+            quotes.reverse()
+
+        with tqdm(total=len(quotes), unit="image", desc="Image Generation") as pbar:
+            for idx, quote in enumerate(quotes):
+                if self.censor and Profanity().check(quote):
+                    print("Profanity found, skipping")
+                    pbar.update(1)
+                    continue
+                ImageMaker(quote).save(str(idx))
+                pbar.update(1)
+    
 
 if __name__ == "__main__":
-    quote().bulkProcess()
+    Process(censor=True).bulk("quotes.txt")
